07043839d8addc1894f92f9b7dcde9541d369129
Randy Forbes <rforbes@salesforce.com>
2018-02-18 15:52:01 -0500

    Added AutoMapper support

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Library.API.Services;
+using System.Collections.Generic;
+using Library.API.Models;
+using Library.API.Helpers;
 
 namespace Library.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/authors")]
     public class AuthorsController : Controller
     {
         private ILibraryRepository _libraryRespository;
@@ -16,7 +19,21 @@ namespace Library.API.Controllers
         public IActionResult GetAuthors()
         {
             var authorsFromRepo = _libraryRespository.GetAuthors();
-            return new JsonResult(authorsFromRepo);
+
+            var authors = new List<AuthorDto>();
+
+            foreach (var author in authorsFromRepo)
+            {
+                authors.Add(new AuthorDto()
+                {
+                    Id = author.Id,
+                    Name = $"{author.FirstName} {author.LastName}",
+                    Genre = author.Genre,
+                    Age = author.DateOfBirth.GetCurrentAge(),
+                });
+            }
+
+            return new JsonResult(authors);
         }
     }
 }
---
+++ Models/AuthorDto.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Library.API.Models
+{
+    public class AuthorDto
+    {
+        public Guid Id;
+        public string Name;
+        public int Age;
+        public string Genre;
+    }
+}
