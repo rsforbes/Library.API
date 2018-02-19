109cb68a687e68743f0a60cce495c07ea14cf59e
Randy Forbes <rforbes@salesforce.com>
2018-02-19 14:32:14 -0500

    Implemented Delete for Book

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -76,5 +76,28 @@ namespace Library.API.Controllers
 
             return CreatedAtRoute("GetBookForAuthor", new {authorId = authorId, id = bookToReturn.Id}, bookToReturn);
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBookForAuthor(Guid authorId, Guid id)
+        {
+            if(!_libraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var bookForAuthorFromRepo = _libraryRepository.GetBookForAuthor(authorId, id);
+            if(bookForAuthorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            _libraryRepository.DeleteBook(bookForAuthorFromRepo);
+            if (!_libraryRepository.Save())
+            {
+                throw new Exception($"Deleting book {id} for author {authorId} failed on save");
+            }
+
+            return NoContent();
+        }
     }
 }
